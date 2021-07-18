7d10f0ef784710c4f2b0c1edc0e7a210e1eca69e
luckpp <marius.andritoi@yahoo.com>
2021-07-18 14:22:57 +0300

    worker ack message

--- rabbitmq/src/02-work/new-task.js
+++ rabbitmq/src/02-work/new-task.js
@@ -15,8 +15,12 @@ async function sendAsync(text) {
     connection = await amqp.connect('amqp://localhost');
     const channel = await connection.createChannel();
     await channel.assertQueue(queueName, {
-      durable: true,
+      durable: true, // make sure that the queue will survive a RabbitMQ node restart
     });
+    // Marking messages as persistent doesn't fully guarantee that a message won't be lost.
+    // Although it tells RabbitMQ to save the message to disk, there is still a short time window
+    // when RabbitMQ has accepted a message and hasn't saved it yet.
+    // If you need a stronger guarantee then you can use publisher confirms.
     channel.sendToQueue(queueName, Buffer.from(message), { persistent: true });
   } catch (err) {
     console.error(err);
--- rabbitmq/src/02-work/worker.js
+++ rabbitmq/src/02-work/worker.js
@@ -2,12 +2,13 @@ const amqp = require('amqplib');
 const uuid = require('uuid');
 const { green } = require('chalk');
 
-function consumer(msg) {
+function consumer(msg, channel) {
   const content = msg.content.toString();
   console.log(` [x] Received ${content}`);
   const contentJson = JSON.parse(content);
   setTimeout(() => {
     console.log(green(' [x] Done'));
+    channel.ack(msg);
   }, contentJson.delay * 1000);
 }
 
@@ -20,8 +21,19 @@ async function receiveAsync() {
     await channel.assertQueue(queueName, {
       durable: true,
     });
-    channel.consume(queueName, consumer, {
-      noAck: true, // automatic acknowledgment mode
+
+    // This tells RabbitMQ not to give more than one message to a worker at a time.  Or, in other words,
+    // don't dispatch a new message to a worker until it has processed and acknowledged the previous one.
+    // Instead, it will dispatch it to the next worker that is not still busy.
+    channel.prefetch(1);
+
+    console.log(
+      ' [*] Waiting for messages in %s. To exit press CTRL+C',
+      queueName
+    );
+
+    channel.consume(queueName, (msg) => consumer(msg, channel), {
+      noAck: false, // manual acknowledgment mode
     });
   } catch (err) {
     console.error(err);
