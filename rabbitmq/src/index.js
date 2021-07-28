0a0333d73c53479168b2ac534511125da001818e
luckpp <marius.andritoi@yahoo.com>
2021-07-28 08:59:14 +0300

    use common lib from JS

--- rabbitmq/src/index.js
+++ rabbitmq/src/index.js
@@ -1,6 +1,10 @@
+const { Dummy } = require('rabbitmq-common');
 const { green } = require('chalk');
 
 const log = console.log;
+const dummy = new Dummy();
+
+dummy.print();
 
 log(green('Hello from index.js!'));
 
